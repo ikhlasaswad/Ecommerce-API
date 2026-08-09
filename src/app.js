c593be9fc5709af36d6061fb794cf5ccf0ae9bfe
ikhlas alaswad <ikhlasaswad252@gmail.com>
2026-08-09 15:07:04 +0300

    chore: initialize ecommerce api project

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "E-Commerce API is running",
+  });
+});
+
+module.exports = app;
